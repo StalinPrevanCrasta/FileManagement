4fe7f8fa752f09a3e3ea2e019b2fa79bdee04acc
ShivaniBS <22c19.shivanib@sjec.ac.in>
2025-01-03 23:59:54 +0530

    .....

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore'],
-        },
-      },
-    },
-  },
-  server: {
-    port: 5173,
-    host: true
+    outDir: 'dist'
   }
 })
