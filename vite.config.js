e5b05d8a0c003dca14353ec7da455bb1b84d3e8e
ShivaniBS <22c19.shivanib@sjec.ac.in>
2025-01-04 00:11:14 +0530

    ....

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    build: {
+      outDir: 'dist',
+      chunkSizeWarningLimit: 1600,
+      rollupOptions: {
+        onwarn(warning, warn) {
+          if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+            return
+          }
+          warn(warning)
+        }
+      }
+    },
+    define: {
+      'process.env': env
+    }
   }
 })
